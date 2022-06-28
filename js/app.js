4e091c8403e2fb4744a916552b8a35715bbaaf84
Nicholas Emmanuel Chimaobi <nicholasemmanuel321@gmail.com>
2022-06-29 00:28:08 +0100

    add js to fetch movie data

---
+++ js/app.js
@@ -0,0 +1,64 @@
+const API_URL =
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=114d2496fece78a34687ca32ab825bda&page=1';
+const ImgSrc = 'https://image.tmdb.org/t/p/w1280';
+const Search_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=114d2496fece78a34687ca32ab825bda&query="';
+
+// =========== getting the value from the dom ===========
+
+const main = document.querySelector('#main');
+const form = document.querySelector('#form');
+const search = document.querySelector('#search');
+
+// =========== get initial movies ===========
+
+getMovies(API_URL);
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  displayMovies(data.results);
+}
+
+function displayMovies(movie) {
+  main.innerHTML = '';
+  movie.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieData = document.createElement('div');
+    movieData.classList.add('movie');
+    movieData.innerHTML = `
+    <img src="${ImgSrc + poster_path}" alt="${title}" />
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${classRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+          <h3>overview</h3>
+          ${overview}
+        </div>
+        `;
+    main.appendChild(movieData);
+  });
+}
+
+function classRate(vote) {
+  if (vote >= 8) {
+    return 'green';
+  } else if (vote >= 5) {
+    return 'orange';
+  } else {
+    return 'red';
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const searchList = search.value;
+  if (searchList && searchList !== '') {
+    getMovies(Search_API + searchList);
+    search.value = '';
+  } else {
+    window.location.reload();
+  }
+});
